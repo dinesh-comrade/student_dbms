84c136864f9faf3508a3562311c244e4e7420b29
Dinesh-comarde <rdineshrdp@gmail.com>
2024-03-15 10:10:27 +0530

    all crud methods completed

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb");
+
 const getItems = (dbCollection) => async (request, reply) => {
   try {
     const students = await dbCollection.find({}).toArray();
@@ -10,7 +12,9 @@ const getItems = (dbCollection) => async (request, reply) => {
 
 const getItem = (dbCollection) => async (request, reply) => {
   try {
-    const student = await dbCollection.findOne({ _id: request.params.id });
+    const student = await dbCollection.findOne({
+      _id: ObjectId(request.params.id),
+    });
     if (!student) {
       return reply.code(404).send({ error: "Student Not Found" });
     }
@@ -23,7 +27,9 @@ const getItem = (dbCollection) => async (request, reply) => {
 
 const postItem = (dbCollection) => async (request, reply) => {
   try {
-    const student = await dbCollection.insertOne(request.body);
+    const newId = new ObjectId().toHexString();
+    const newItem = { _id: newId, ...request.body };
+    const student = await dbCollection.insertOne(newItem);
     return reply.code(201).send(student);
   } catch (error) {
     console.error("Error fetching student:", error);
@@ -31,4 +37,35 @@ const postItem = (dbCollection) => async (request, reply) => {
   }
 };
 
-module.exports = { getItems, getItem, postItem };
+const updateItem = (dbCollection) => async (request, reply) => {
+  try {
+    const id = ObjectId.createFromHexString(request.params.id);
+    const result = await dbCollection.updateOne(
+      { _id: id },
+      { $set: request.body }
+    );
+    if (result.matchedCount === 0) {
+      return reply.code(404).send({ error: "Student Not Found" });
+    }
+    return reply.code(200).send(result);
+  } catch (error) {
+    console.error("Error updating student:", error);
+    return reply.code(500).send({ error: "Internal Server Error" });
+  }
+};
+
+const deleteItem = (dbCollection) => async (request, reply) => {
+  try {
+    const id = ObjectId.createFromHexString(request.params.id);
+    const result = await dbCollection.deleteOne({ _id: id });
+    if (result.deletedCount === 0) {
+      return reply.code(404).send({ error: "Student Not Found" });
+    }
+    return reply.code(200).send(result);
+  } catch (error) {
+    console.error("Error deleting student:", error);
+    return reply.code(500).send({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { getItems, getItem, postItem, updateItem, deleteItem };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,10 @@
-const { getItems, getItem, postItem } = require("../controllers/controllers");
+const {
+  getItems,
+  getItem,
+  postItem,
+  updateItem,
+  deleteItem,
+} = require("../controllers/controllers");
 const start = require("../database/database");
 
 async function routes(fastify, options) {
@@ -6,6 +12,8 @@ async function routes(fastify, options) {
   fastify.get("/students", getItems(dbCollection));
   fastify.get("/student/:id", getItem(dbCollection));
   fastify.post("/student", postItem(dbCollection));
+  fastify.put("/student/:id", updateItem(dbCollection));
+  fastify.delete("/student/:id", deleteItem(dbCollection));
 }
 
 module.exports = routes;
