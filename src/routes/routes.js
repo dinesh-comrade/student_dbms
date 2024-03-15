23687352a7238f8706114000760ba2d0eefe1660
Dinesh-comarde <rdineshrdp@gmail.com>
2024-03-15 12:38:42 +0530

    schema updated

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,49 +17,40 @@ const start = require("../database/database");
 
 async function routes(fastify, options) {
   const dbCollection = await start();
-
-  fastify.get("/students", getItemsOpt, getItems(dbCollection));
-  fastify.get("/student/:id", getItemOpt, getItem(dbCollection));
-  fastify.post("/student", postItemOpt, postItem(dbCollection), {
-    abortEarly: false,
+  fastify.route({
+    method: "GET",
+    url: "/students",
+    schema: getItemsOpt,
+    handler: getItems(dbCollection),
   });
-  fastify.put("/student/:id", updateItemOpt, updateItem(dbCollection));
-  fastify.delete("/student/:id", deleteItemOpt, deleteItem(dbCollection));
 
-  // fastify.route({
-  //   method: "GET",
-  //   url: "/students",
-  //   schema: getItemsOpt,
-  //   handler: getItems(dbCollection),
-  // });
+  fastify.route({
+    method: "GET",
+    url: "/student/:id",
+    schema: getItemOpt,
+    handler: getItem(dbCollection),
+  });
 
-  // fastify.route({
-  //   method: "GET",
-  //   url: "/student/:id",
-  //   schema: getItemOpt,
-  //   handler: getItem(dbCollection),
-  // });
+  fastify.route({
+    method: "POST",
+    url: "/student",
+    schema: postItemOpt,
+    handler: postItem(dbCollection),
+  });
 
-  // fastify.route({
-  //   method: "POST",
-  //   url: "/student",
-  //   schema: postItemOpt,
-  //   handler: postItem(dbCollection),
-  // });
+  fastify.route({
+    method: "PUT",
+    url: "/student/:id",
+    schema: updateItemOpt,
+    handler: updateItem(dbCollection),
+  });
 
-  // fastify.route({
-  //   method: "PUT",
-  //   url: "/student/:id",
-  //   schema: updateItemOpt,
-  //   handler: updateItem(dbCollection),
-  // });
-
-  // fastify.route({
-  //   method: "DELETE",
-  //   url: "/student/:id",
-  //   schema: deleteItemOpt,
-  //   handler: deleteItem(dbCollection),
-  // });
+  fastify.route({
+    method: "DELETE",
+    url: "/student/:id",
+    schema: deleteItemOpt,
+    handler: deleteItem(dbCollection),
+  });
 }
 
 module.exports = routes;
